55334a74b1aaef43a99924c4004a8d205ffa64d1
kennedy mwangi <kennedy.mwangi@equitybank.co.ke>
2024-10-22 09:15:18 +0300

    initial set up from edit

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rjshkhr.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: '/',
+  title: 'Portfolio.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Kennedy Mwangi',
+  role: 'Full stack Engineer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+    'Am a full stack engineer with four years of experience.',
   resume: 'https://example.com',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/mwangikibui/',
+    github: 'https://github.com/mwangiKibui',
   },
 }
 
@@ -21,28 +21,28 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Chamasoft',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
+      'Investment group management software.',
+    stack: ['PHP','Jenkins','Github Web Hooks','Jenkins'],
     sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+    livePreview: 'https://chamasoft.com',
   },
   {
-    name: 'Project 2',
+    name: 'Websacco',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
+      'Financial solution for small to medium sized saccos.',
+    stack: ['PHP', 'Jenkins', 'Github Web Hooks', 'Jenkins'],
     sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+    livePreview: 'https://websacco.com',
   },
   {
-    name: 'Project 3',
+    name: 'JustPostIt',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
+      'A fake HTTP client for simulating POST requests',
+    stack: ['Supabase', 'NextJS'],
     sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+    livePreview: 'https://justpostit.dev',
   },
 ]
 
@@ -55,17 +55,14 @@ const skills = [
   'TypeScript',
   'React',
   'Redux',
-  'SASS',
-  'Material UI',
   'Git',
   'CI/CD',
-  'Jest',
-  'Enzyme',
+  'ASP.Net'
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'kenmwangi250@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
